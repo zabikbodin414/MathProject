5925ce57c57f35cd9f7f3c0b36e6bdfefa363601
zabikbodin414 <zabikbodin414@gmail.com>
2025-03-31 07:50:36 +0000

    Add 20.js

---
+++ src/20.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y === 0) throw new Error('Cannot divide by zero');
+  return x / y;
+}
