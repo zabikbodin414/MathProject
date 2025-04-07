bf8af5e4587ab36db709a56e8eb591610769a1f9
zabikbodin414 <zabikbodin414@gmail.com>
2025-04-07 14:33:59 +0000

    Add 25.js

---
+++ src/25.js
@@ -0,0 +1,16 @@
+// 代码开始
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+console.log(isPrime(17)); // 应该输出 true
