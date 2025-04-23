e89f4fc14a685bb8229404e7d242fb1e2481ec56
zabikbodin414 <zabikbodin414@gmail.com>
2025-04-23 11:18:44 +0000

    Add 29.js

---
+++ src/29.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const maxValue = 100;
+console.log(getRandomInt(maxValue)); // Output a random integer between 0 and maxValue-1
