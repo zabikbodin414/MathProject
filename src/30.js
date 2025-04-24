48e90b5f9389b8fc51960471d0ec91107bcc7ed1
zabikbodin414 <zabikbodin414@gmail.com>
2025-04-24 15:10:50 +0000

    Add 30.js

---
+++ src/30.js
@@ -0,0 +1,6 @@
+function squareRoot(num) {
+    const sqrt = Math.sqrt(num);
+    console.log(`The square root of ${num} is approximately ${sqrt.toFixed(2)}`);
+}
+
+squareRoot(16); // Replace 16 with another number to test the function
