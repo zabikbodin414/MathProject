41fd5731ae043097b9ca38e4eafd046a2cd7db64
zabikbodin414 <zabikbodin414@gmail.com>
2025-05-20 07:32:41 +0000

    Add 35.js

---
+++ src/35.js
@@ -0,0 +1,8 @@
+// Code Example 1
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Code Example 2
+const values = [3, 4, 5];
+console.log(`Sum of ${values[0]} and ${values[1]} is: ${addNumbers(values[0], values[1])}`);
